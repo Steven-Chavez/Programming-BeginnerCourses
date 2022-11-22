9b2c232ef8a8e321a1d217c21f9f56f6b4bb9d20
Steven-Chavez <schavez256@yahoo.com>
2022-11-22 21:37:46 +0000

    new file:   02-Comments.go

---
+++ Go/02-Comments.go
@@ -0,0 +1,18 @@
+// Author: Steven Chavez
+// FileName: 02-Comments.go
+// Creation Date: 11/22/2022
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("View code to see comments!")
+	//This is a one line comment
+
+	/*
+		This is a
+		Multiple line
+		Comment!
+	*/
+}
