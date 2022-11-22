4a9a447eb76fa9fc12bb96acee7d934b9118d874
Steven-Chavez <schavez256@yahoo.com>
2022-11-22 21:26:41 +0000

    new file:   Go/01-HelloWorld.go

---
+++ Go/01-HelloWorld.go
@@ -0,0 +1,11 @@
+// Author: Steven Chavez
+// FileName: 01-HelloWorld.go
+// Creation Date: 11/22/2022
+package main
+
+import "fmt"
+
+func main() {
+	// Print "Hello World to the terminal
+	fmt.Println("Hello World")
+}
