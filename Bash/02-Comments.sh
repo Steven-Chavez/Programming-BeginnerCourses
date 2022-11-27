b635123824a4c3bf89747d6a069acd5251adb50f
Steven-Chavez <schavez256@yahoo.com>
2022-11-27 04:48:48 +0000

    new file:   02-Comments.sh

---
+++ Bash/02-Comments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author: Steven Chavez
+# FileName: 
+# Creation Date: 11/22/2022
+
+# This is a single line comment
+
+echo "https://exposingthreats.com" #Inline comment
+
+<<comment
+echo "exposing threats"
+echo "This is a multi-line Bash comment
+This will not be displayed
+comment
