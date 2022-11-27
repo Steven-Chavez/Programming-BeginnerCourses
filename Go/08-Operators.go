b38fb530f24778decaf609d7b945ced3baf714cf
Steven-Chavez <schavez256@yahoo.com>
2022-11-27 18:28:17 +0000

    new file:   08-Operators.go

---
+++ Go/08-Operators.go
@@ -0,0 +1,32 @@
+// Author: Steven Chavez
+// FileName: 08-Operators.go
+// Creation Date: 11/27/2022
+//
+// OPERATORS DEF
+// Used to perform operations on variables and values// like math, comparing, equals to, ect
+
+package main
+
+import "fmt"
+
+func main() {
+	// Variables
+	var a int = 10
+	var b int = 50
+	var c int = 100
+
+	// ARITHMETIC OPERATORS
+	// Used to perform math operations
+	//
+	// + Addition
+	// - Subtraction
+	// * Multiplcation
+	// / Division
+	// % Modulas
+	// ++ Increment
+	// -- Decrement
+
+	fmt.Println(a + a)
+	fmt.Println((c + c) / 10)
+	fmt.Println(a * b)
+}
