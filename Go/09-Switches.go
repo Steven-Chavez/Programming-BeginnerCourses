e8d9e90f667aaca1311a4b73689957a7dcb486c3
Steven-Chavez <schavez256@yahoo.com>
2022-11-28 19:30:23 +0000

    new file:   09-Switches.go

---
+++ Go/09-Switches.go
@@ -0,0 +1,47 @@
+// Author: Steven Chavez
+// FileName: 09-Switches.go
+// Creation Date: 11/28/2022
+//
+// SWITCH DEF
+// Runs blocks of code based on matched values
+//
+// FORMAT
+// switch expression {
+// case x:
+//    // code block
+// case y:
+//    // code block
+// case z:
+// ...
+// default: // Optional - Used if no case matches
+//    // code block
+// }
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// variables
+	var i int
+	var modulas int
+
+	// Print out directions
+	fmt.Print("\n", "Enter a number to see if it's even or odd: ")
+
+	// Read user input
+	fmt.Scanln(&i)
+
+	// If modulas equals zero the number is even
+	modulas = i % 2
+
+	// Switch statment
+	switch modulas {
+	case 0:
+		fmt.Println(i, "- is even")
+	default:
+		fmt.Println(i, "- is odd")
+
+	}
+}
