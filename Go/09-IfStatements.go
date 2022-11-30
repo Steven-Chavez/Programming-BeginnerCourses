d317257becab9e84886c5240f92b51a915789937
Steven-Chavez <schavez256@yahoo.com>
2022-11-30 12:59:18 +0000

    Added definition and syntax to comment header

--- Go/09-IfStatements.go
+++ Go/09-IfStatements.go
@@ -1,3 +1,19 @@
+// Author: Steven Chavez
+// FileName: 09-IfStatements.go
+// Creation Date: 11/30/2022
+//
+// IF STATEMENT DEFINITION
+// Executes a block of code if a condition is met.
+//
+// SYNTAX
+// if condition {
+// 	// code to be executed
+// } else if condition2 {
+// 	// code to be executed
+// } else {
+// 	// code to be executed if no condition is met
+// }
+
 package main
 
 import "fmt"
