b0ece1048f6cec2a8aafb2b2164016c45c946cb4
Steven-Chavez <schavez256@yahoo.com>
2022-12-06 17:05:32 +0000

    Learning how to make REST API calls in go

---
+++ Go/13-CallRestAPI.go
@@ -0,0 +1,44 @@
+// https://www.soberkoder.com/consume-rest-api-go/
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// Todo struct
+type Todo struct {
+	UserID    int    `json:"userId"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func get() {
+	fmt.Println("1. Performing Http Get...")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	// Convert response body to string
+	bodyString := string(bodyBytes)
+	fmt.Println("API Response as String:\n" + bodyString)
+
+	// Convert response body to Todo struct
+	var todoStruct Todo
+	json.Unmarshal(bodyBytes, &todoStruct)
+	fmt.Printf("API Response as struct %+v\n", todoStruct)
+}
+
+func main() {
+	get()
+}
