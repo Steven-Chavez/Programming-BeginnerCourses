8820f604ee5001eac39c516fd3e1beff35132bda
Steven-Chavez <schavez256@yahoo.com>
2022-12-07 00:17:42 +0000

    Re-organized scripts with directories

--- Go/REST-API/02-ipinfo.go
+++ Go/REST-API/02-ipinfo.go
@@ -1,13 +1,13 @@
-// https://www.soberkoder.com/consume-rest-api-go/
-
+//https://ipinfo.io/account/home
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 // Todo struct
@@ -23,9 +23,22 @@ type ipinfo struct {
 	Timezone string `json:"timezone"`
 }
 
-func get() {
-	fmt.Println("1. Performing Http Get...")
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+func get(ip string) {
+	var err error
+	var token []byte
+	var url string
+	var resp *http.Response
+
+	token, err = os.ReadFile("/home/steven/api/ipinfo.txt")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	url = "https://ipinfo.io/" + ip + "?token=" + string(token)
+	url = strings.Replace(url, "\n", "", -1)
+
+	resp, err = http.Get(url)
 
 	if err != nil {
 		log.Fatalln(err)
@@ -38,12 +51,34 @@ func get() {
 	bodyString := string(bodyBytes)
 	fmt.Println("API Response as String:\n" + bodyString)
 
-	// Convert response body to Todo struct
-	var todoStruct Todo
-	json.Unmarshal(bodyBytes, &todoStruct)
-	fmt.Printf("API Response as struct %+v\n", todoStruct)
+	/*
+		defer resp.Body.Close()
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println("1. Performing Http Get...")
+		resp, err = http.Get("https://jsonplaceholder.typicode.com/todos/1")
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+		// Convert response body to string
+		bodyString := string(bodyBytes)
+		fmt.Println("API Response as String:\n" + bodyString)
+
+		// Convert response body to Todo struct
+		var todoStruct Todo
+		json.Unmarshal(bodyBytes, &todoStruct)
+		fmt.Printf("API Response as struct %+v\n", todoStruct)*/
 }
 
 func main() {
-	get()
+
+	var ip string
+
+	fmt.Print("Enter an IP: ")
+	fmt.Scanln(&ip)
+	get(ip)
 }
