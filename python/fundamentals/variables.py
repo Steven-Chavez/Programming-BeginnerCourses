6f4da4fc2e66623336aa2f6d37bd57dacb00c39a
Steven Chavez <schavez256@yahoo.com>
2024-04-16 17:02:22 -0600

    Update variables.py

--- python/fundamentals/variables.py
+++ python/fundamentals/variables.py
@@ -1,4 +1,14 @@
 """
 Author: Steven Chavez
-Email: 
+Filename: variables.py
+Date: 4/16/2024
 """
+
+# Variables can change types
+v = 0
+v = "Python"
+
+# Casting variables - specifies the type instead of being infered like above
+v = int(0)      # 0
+v = str(0)      # "0"
+v = float(0)    # 0.0
