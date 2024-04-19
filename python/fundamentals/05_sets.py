21ea38084e627742a5d462d6458368a4edf5deca
Steven Chavez <schavez256@yahoo.com>
2024-04-18 21:16:48 -0600

    Create 05_sets.py

---
+++ python/fundamentals/05_sets.py
@@ -0,0 +1,19 @@
+"""
+Author: Steven Chavez
+Filename: 03_lists.py
+Date: 4/17/2024
+References: w3schools.com/python
+
+Sets are used to store multiple items in a single variable.
+Set is one of 4 built-in data types in Python used to store collections of data, 
+the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
+A set is a collection which is unordered, unchangeable*, and unindexed.
+
+
+"""
+
+###################
+### Python Sets ###
+###################
+
+# Sets are written with curly brackets.
