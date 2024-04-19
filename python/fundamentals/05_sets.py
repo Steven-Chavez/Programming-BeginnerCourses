8be63fd08d7cccf15f127ae55098a374b3dd526c
Steven Chavez <schavez256@yahoo.com>
2024-04-18 21:23:26 -0600

    Update 05_sets.py

--- python/fundamentals/05_sets.py
+++ python/fundamentals/05_sets.py
@@ -9,7 +9,12 @@ Set is one of 4 built-in data types in Python used to store collections of data,
 the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
 A set is a collection which is unordered, unchangeable*, and unindexed.
 
+Set items are unordered, unchangeable, and do not allow duplicate values.
 
+Unordered means that the items in a set do not have a defined order.
+Set items can appear in a different order every time you use them, and cannot be referred to by index or key.
+
+Set items are unchangeable, meaning that we cannot change the items after the set has been created.
 """
 
 ###################
@@ -17,3 +22,23 @@ A set is a collection which is unordered, unchangeable*, and unindexed.
 ###################
 
 # Sets are written with curly brackets.
+s = {"Tomatoes", "Peppers", "Kale"}
+print(s) # prints {'Kale', 'Tomatoes', 'Peppers'}
+
+# Sets cannot have two items with the same value. Duplicate values will be ignored
+s = {"Tomatoes", "Peppers", "Kale", "Kale"}
+print(s) # prints {'Kale', 'Tomatoes', 'Peppers'}
+
+# True and 1 are considered the same value. Same with False and 0.
+s = {"Tomatoes", "Peppers", "Kale", True, 1, 2}
+print(s) # prints {True, 2, 'Kale', 'Tomatoes', 'Peppers'}
+
+# Set items can be any data type and contain different data types
+s = {"Tomatoes", "Peppers", "Kale", True, 1, 2}
+
+# Sets are defined as the data type set
+print(type(s)) # prints <class 'set'>
+
+
+
+
