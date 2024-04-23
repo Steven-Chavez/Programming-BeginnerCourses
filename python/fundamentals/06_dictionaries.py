75cbdac573d284e2038ad051f78333b285fcdf6f
Steven Chavez <schavez256@yahoo.com>
2024-04-22 20:58:26 -0600

    Update 06_dictionaries.py

--- python/fundamentals/06_dictionaries.py
+++ python/fundamentals/06_dictionaries.py
@@ -286,9 +286,32 @@ print(nd) # prints {'item1': {'brand': 'Cold Steel', 'model': 'SRK', 'type': 'kn
 # To access items from a nested dictionary, you use the name of the dictionaries, starting with the outer dictionary
 print(nd["item3"]["model"]) # prints Demko Hawk
 
+# You can loop through a dictionary by using the items() method like this
+for x, obj in nd.items():
+    print(x)
+    for y in obj:
+      print(y + ':', obj[y])
 
 
+##########################
+### Dictionary Methods ###
+##########################
+"""
+Python has a set of built-in methods that you can use on dictionaries.
 
+Method	Description
+clear() -	Removes all the elements from the dictionary
+copy() - Returns a copy of the dictionary
+fromkeys() - Returns a dictionary with the specified keys and value
+get() - Returns the value of the specified key
+items() - Returns a list containing a tuple for each key value pair
+keys() - Returns a list containing the dictionary's keys
+pop() - Removes the element with the specified key
+popitem() - Removes the last inserted key-value pair
+setdefault() - Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update() - Updates the dictionary with the specified key-value pairs
+values() - Returns a list of all the values in the dictionary
+"""
 
 
 
