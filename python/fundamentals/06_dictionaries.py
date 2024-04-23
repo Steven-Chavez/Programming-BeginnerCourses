0201215cb430f60ce1a5fe860a6b84ff445f61de
Steven Chavez <schavez256@yahoo.com>
2024-04-22 20:33:28 -0600

    Update 06_dictionaries.py

--- python/fundamentals/06_dictionaries.py
+++ python/fundamentals/06_dictionaries.py
@@ -118,6 +118,31 @@ d.update({"model": "AD-15 Lite"})
 print(d) # prints {'brand': 'Cold Steel', 'model': 'AD-15 Lite', 'type': 'knife'}
 
 
+#################
+### Add items ###
+#################
+
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it
+d = {
+  "brand": "Cold Steel",
+  "model": "SRK",
+  "type": "knife"
+}
+d["color"] = "Black/Stainless"
+print(d) # prints {'brand': 'Cold Steel', 'model': 'SRK', 'type': 'knife', 'color': 'Black/Stainless'}
+
+# The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
+# The argument must be a dictionary, or an iterable object with key:value pairs.
+d = {
+  "brand": "Cold Steel",
+  "model": "SRK",
+  "type": "knife"
+}
+d.update({"color": "Black/Stainless"})
+print(d) # prints {'brand': 'Cold Steel', 'model': 'SRK', 'type': 'knife', 'color': 'Black/Stainless'}
+
+
+
 
 
 
