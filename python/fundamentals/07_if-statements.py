f6dc16ff2aa177d78b68c572d58513cb2db98d1c
Steven Chavez <schavez256@yahoo.com>
2024-04-23 22:00:31 -0600

    Update 07_if-statements.py

--- python/fundamentals/07_if-statements.py
+++ python/fundamentals/07_if-statements.py
@@ -44,3 +44,26 @@ elif x == y:
   print("x is the same as y") # does not print because condition is false
 else:
   print("x is less than y") # prints x is less than y
+
+# One line if statement
+x = 0
+y = 20
+if x < y: print("x is less than y") # prints x is less than y
+
+# One line if else statement:
+x = 20
+y = 0
+print('x') if x < y else print('y') # prints y
+
+# One line if else statement, with 3 conditions
+x = 20
+y = 40
+print("x is greater than y") if x > y else print("x is the same as y") if x == y else print("x is less than y") # prints x is less than y
+
+x = 20
+y = 20
+print("x is greater than y") if x > y else print("x is the same as y") if x == y else print("x is less than y") # prints x is the same as y
+
+x = 20
+y = 10
+print("x is greater than y") if x > y else print("x is the same as y") if x == y else print("x is less than y") # prints x is greater than y
