97f9412a6a744d8c60d8cb0a3d76b45a9647524d
Steven Chavez <schavez256@yahoo.com>
2024-04-23 21:50:13 -0600

    Update 07_if-statements.py

--- python/fundamentals/07_if-statements.py
+++ python/fundamentals/07_if-statements.py
@@ -4,4 +4,43 @@ Filename: 03_lists.py
 Date: 4/23/2024
 References: w3schools.com/python
 
+Python supports the usual logical conditions from mathematics:
+- Equals: a == b
+- Not Equals: a != b
+- Less than: a < b
+- Less than or equal to: a <= b
+- Greater than: a > b
+- Greater than or equal to: a >= b
+
+These conditions can be used in several ways, most commonly in "if statements" and loops.
+An "if statement" is written by using the if keyword.
+
 """
+
+#################
+### If...Else ###
+#################
+
+# If statement format
+x = 0
+y = 20
+if x < y:
+  print("x is less than y") # prints x is less than y
+
+# The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition"
+x = 0
+y = 0
+if x > y:
+  print("x is greater than y") # does not print because condition is false
+elif x == y:
+  print("x is the same as y") # prints x is the same as y
+
+# The else keyword catches anything which isn't caught by the preceding conditions.
+x = -1
+y = 0
+if x > y:
+  print("x is greater than y") # does not print because condition is false
+elif x == y:
+  print("x is the same as y") # does not print because condition is false
+else:
+  print("x is less than y") # prints x is less than y
