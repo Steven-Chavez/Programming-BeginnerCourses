4a644514dd9df519ffebce51738f6c8990d03886
Steven Chavez <schavez256@yahoo.com>
2024-04-23 22:25:11 -0600

    Create 08_while-loops.py

---
+++ python/fundamentals/08_while-loops.py
@@ -0,0 +1,11 @@
+"""
+Author: Steven Chavez
+Filename: 08_while-loops.py
+Date: 4/17/2024
+References: w3schools.com/python
+
+Python has two primitive loop commands:
+
+- while loops
+- for loops
+"""
